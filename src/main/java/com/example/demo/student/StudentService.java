c79d6a2c5cd920cf7494e2e78a07675404ad71a1
Muneeb Sheikh <imuneebofficial@gmail.com>
2021-07-27 04:51:49 +0500

    deleteMapping & PutMapping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -37,6 +38,30 @@ import java.util.Optional;
             throw new IllegalStateException("Email Taken");
         }
         repository.save(student);
+
+    }
+
+    public void deleteStudent(Long studentId) {
+        if(!repository.existsById(studentId))
+            throw new IllegalStateException("Student with id {" + studentId + "} does not exist");
+        repository.deleteById(studentId);
+
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = repository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id {"+ studentId +"} does not exist"));
+
+        if(name != null && !name.isBlank() && !name.equals(student.getName())){
+            student.setName(name);
+        }
+        if(email != null && !email.equals(student.getEmail()) && !email.isBlank()){
+            Optional<Student> studentOptional = repository.findStudentByEmail(email);
+            if(studentOptional.isPresent())
+                throw new IllegalStateException("Email is Taken");
+            student.setEmail(email);
+        }
 
     }
 }
